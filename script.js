658a463a615a3edea84218d1fb1b3107fcd006df
Davincii254 <victorkuria7@gmail.com>
2026-01-29 13:52:30 +0300

    update

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 // --- SETUP ---
+const playerDisplay = document.getElementById("playerDisplay");
+const computerDisplay = document.getElementById("computerDisplay");
+const resultDisplay = document.getElementById("resultDisplay");
+const playerScoreDisplay = document.getElementById("playerScoreDisplay");
+const computerScoreDisplay = document.getElementById("computerScoreDisplay");
+
+
 const choices = ["rock", "paper", "scissors"];
+
 const winConditions = {
     rock: "scissors",
     paper: "rock",
     scissors: "paper"
 };
 
-const playerDisplay = document.getElementById("playerDisplay");
-const computerDisplay = document.getElementById("computerDisplay");
-const resultDisplay = document.getElementById("resultDisplay");
-const playerScoreDisplay = document.getElementById("playerScoreDisplay");
-const computerScoreDisplay = document.getElementById("computerScoreDisplay");
+
 
 let playerScore = 0;
 let computerScore = 0;
